1019f57fd0935e1fc71871278b03a2dba8beeee8
ouzm <wjj315315@gmail.com>
2022-02-14 15:11:59 +0800

    fix bsc05, bsc06

--- contracts/sales/BrewerySale.sol
+++ contracts/sales/BrewerySale.sol
@@ -122,7 +122,10 @@ contract BrewerySale is ReentrancyGuard {
     // Constructor, always initialized through SalesFactory
     constructor(address _admin, address _allocationStaking) public {
         require(_admin != address(0), "_admin != address(0)");
-        require(_allocationStaking != address(0), "_allocationStaking != address(0)");
+        require(
+            _allocationStaking != address(0),
+            "_allocationStaking != address(0)"
+        );
         admin = IAdmin(_admin);
         factory = ISalesFactory(msg.sender);
         allocationStakingContract = IAllocationStaking(_allocationStaking);
@@ -136,10 +139,17 @@ contract BrewerySale is ReentrancyGuard {
     ) external onlyAdmin {
         require(
             vestingPercentPerPortion.length == 0 &&
-            vestingPortionsUnlockTime.length == 0, "vestingPerPortion params not provided"
+                vestingPortionsUnlockTime.length == 0,
+            "vestingPerPortion params not provided"
         );
-        require(_unlockingTimes.length == _percents.length, "_unlockingTimes.length == _percents.length");
-        require(portionVestingPrecision > 0, "Safeguard for making sure setSaleParams get first called.");
+        require(
+            _unlockingTimes.length == _percents.length,
+            "_unlockingTimes.length == _percents.length"
+        );
+        require(
+            portionVestingPrecision > 0,
+            "Safeguard for making sure setSaleParams get first called."
+        );
         require(_maxVestingTimeShift <= 30 days, "Maximal shift is 30 days.");
 
         // Set max vesting time shift
@@ -157,8 +167,8 @@ contract BrewerySale is ReentrancyGuard {
     }
 
     function shiftVestingUnlockingTimes(uint256 timeToShift)
-    external
-    onlyAdmin
+        external
+        onlyAdmin
     {
         require(
             timeToShift > 0 && timeToShift < maxVestingTimeShift,
@@ -193,10 +203,10 @@ contract BrewerySale is ReentrancyGuard {
         );
         require(
             _tokenPriceInETH != 0 &&
-            _amountOfTokensToSell != 0 &&
-            _saleEnd > block.timestamp &&
-            _tokensUnlockTime > block.timestamp &&
-            _maxParticipation > 0,
+                _amountOfTokensToSell != 0 &&
+                _saleEnd > block.timestamp &&
+                _tokensUnlockTime > block.timestamp &&
+                _maxParticipation > 0,
             "setSaleParams: Bad input"
         );
         require(_portionVestingPrecision >= 100, "Should be at least 100");
@@ -225,16 +235,13 @@ contract BrewerySale is ReentrancyGuard {
     // @notice     Function to retroactively set sale token address, can be called only once,
     //             after initial contract creation has passed. Added as an options for teams which
     //             are not having token at the moment of sale launch.
-    function setSaleToken(
-        address saleToken
-    )
-    external
-    onlyAdmin
-    {
-        require(address(sale.token) == address(0), "Token address can not be zero address");
+    function setSaleToken(address saleToken) external onlyAdmin {
+        require(
+            address(sale.token) == address(0),
+            "Token address can not be zero address"
+        );
         sale.token = IERC20(saleToken);
     }
-
 
     /// @notice     Function to set registration period parameters
     function setRegistrationTime(
@@ -242,15 +249,25 @@ contract BrewerySale is ReentrancyGuard {
         uint256 _registrationTimeEnds
     ) external onlyAdmin {
         require(sale.isCreated, "sale is not created");
-        require(registration.registrationTimeStarts == 0, "registrationTimeStarts should be 0");
+        require(
+            registration.registrationTimeStarts == 0,
+            "registrationTimeStarts should be 0"
+        );
         require(
             _registrationTimeStarts >= block.timestamp &&
-            _registrationTimeEnds > _registrationTimeStarts, "registration time was wrongly set"
+                _registrationTimeEnds > _registrationTimeStarts,
+            "registration time was wrongly set"
         );
-        require(_registrationTimeEnds < sale.saleEnd, "_registrationTimeEnds < sale.saleEnd is not allowed");
+        require(
+            _registrationTimeEnds < sale.saleEnd,
+            "_registrationTimeEnds < sale.saleEnd is not allowed"
+        );
 
         if (sale.saleStart > 0) {
-            require(_registrationTimeEnds < sale.saleStart, "registrationTimeEnds >= sale.saleStart is not allowed");
+            require(
+                _registrationTimeEnds < sale.saleStart,
+                "registrationTimeEnds >= sale.saleStart is not allowed"
+            );
         }
 
         registration.registrationTimeStarts = _registrationTimeStarts;
@@ -262,14 +279,21 @@ contract BrewerySale is ReentrancyGuard {
         );
     }
 
-    function setSaleStart(
-        uint256 starTime
-    ) external onlyAdmin {
+    function setSaleStart(uint256 starTime) external onlyAdmin {
         require(sale.isCreated, "sale is not created.");
         require(sale.saleStart == 0, "setSaleStart: starTime is set already.");
-        require(starTime > registration.registrationTimeEnds, "start time should greater than registrationTimeEnds.");
-        require(starTime < sale.saleEnd, "start time should less than saleEnd time");
-        require(starTime >= block.timestamp, "start time should be in the future.");
+        require(
+            starTime > registration.registrationTimeEnds,
+            "start time should greater than registrationTimeEnds."
+        );
+        require(
+            starTime < sale.saleEnd,
+            "start time should less than saleEnd time"
+        );
+        require(
+            starTime >= block.timestamp,
+            "start time should be in the future."
+        );
         sale.saleStart = starTime;
 
         // Fire event
@@ -278,22 +302,17 @@ contract BrewerySale is ReentrancyGuard {
 
     /// @notice     Registration for sale.
     /// @param      signature is the message signed by the backend
-    function registerForSale(bytes memory signature, uint256 pid)
-    external
-    {
+    function registerForSale(bytes memory signature, uint256 pid) external {
         require(
             block.timestamp >= registration.registrationTimeStarts &&
-            block.timestamp <= registration.registrationTimeEnds,
+                block.timestamp <= registration.registrationTimeEnds,
             "Registration gate is closed."
         );
         require(
             checkRegistrationSignature(signature, msg.sender),
             "Invalid signature"
         );
-        require(
-            !isRegistered[msg.sender],
-            "User can not register twice."
-        );
+        require(!isRegistered[msg.sender], "User can not register twice.");
         isRegistered[msg.sender] = true;
 
         // Lock users stake
@@ -321,10 +340,7 @@ contract BrewerySale is ReentrancyGuard {
 
     /// @notice     Admin function to postpone the sale
     function postponeSale(uint256 timeToShift) external onlyAdmin {
-        require(
-            block.timestamp < sale.saleStart,
-            "sale already started."
-        );
+        require(block.timestamp < sale.saleStart, "sale already started.");
         //  postpone registration start time
         sale.saleStart = sale.saleStart.add(timeToShift);
         require(
@@ -336,25 +352,18 @@ contract BrewerySale is ReentrancyGuard {
     /// @notice     Function to extend registration period
     function extendRegistrationPeriod(uint256 timeToAdd) external onlyAdmin {
         require(
-            registration.registrationTimeEnds.add(timeToAdd) <
-            sale.saleStart,
+            registration.registrationTimeEnds.add(timeToAdd) < sale.saleStart,
             "Registration period overflows sale start."
         );
 
         registration.registrationTimeEnds = registration
-        .registrationTimeEnds
-        .add(timeToAdd);
+            .registrationTimeEnds
+            .add(timeToAdd);
     }
 
     /// @notice     Admin function to set max participation before sale start
-    function setCap(uint256 cap)
-    external
-    onlyAdmin
-    {
-        require(
-            block.timestamp < sale.saleStart,
-            "sale already started."
-        );
+    function setCap(uint256 cap) external onlyAdmin {
+        require(block.timestamp < sale.saleStart, "sale already started.");
 
         require(cap > 0, "Can't set max participation to 0");
 
@@ -365,9 +374,7 @@ contract BrewerySale is ReentrancyGuard {
 
     // Function for owner to deposit tokens, can be called only once.
     function depositTokens() external onlySaleOwner nonReentrant {
-        require(
-            !sale.tokensDeposited, "Deposit can be done only once"
-        );
+        require(!sale.tokensDeposited, "Deposit can be done only once");
 
         sale.tokensDeposited = true;
 
@@ -379,38 +386,30 @@ contract BrewerySale is ReentrancyGuard {
     }
 
     // Function to participate in the sales
-    function participate(
-        bytes memory signature,
-        uint256 amount
-    ) external payable {
-
+    function participate(bytes memory signature, uint256 amount)
+        external
+        payable
+    {
         require(
             amount <= sale.maxParticipation,
             "Overflowing maximal participation for sale."
         );
-        
-        require( sale.tokensDeposited, "Token has not been deposited");
+
+        require(sale.tokensDeposited, "Token has not been deposited");
 
         // User must have registered for the round in advance
-        require(
-            isRegistered[msg.sender],
-            "Not registered for this sale."
-        );
+        require(isRegistered[msg.sender], "Not registered for this sale.");
 
         // Verify the signature
         require(
-            checkParticipationSignature(
-                signature,
-                msg.sender,
-                amount
-            ),
+            checkParticipationSignature(signature, msg.sender, amount),
             "Invalid signature. Verification failed"
         );
 
         // Verify the timestamp
         require(
-            block.timestamp >= sale.saleStart &&
-            block.timestamp < sale.saleEnd, "sale didn't start or it's ended."
+            block.timestamp >= sale.saleStart && block.timestamp < sale.saleEnd,
+            "sale didn't start or it's ended."
         );
 
         // Check user haven't participated before
@@ -420,8 +419,9 @@ contract BrewerySale is ReentrancyGuard {
         require(msg.sender == tx.origin, "Only direct contract calls.");
 
         // Compute the amount of tokens user is buying
-        uint256 amountOfTokensBuying =
-        (msg.value).mul(uint(10) ** IERC20Metadata(address(sale.token)).decimals()).div(sale.tokenPriceInETH);
+        uint256 amountOfTokensBuying = (msg.value)
+            .mul(uint256(10)**IERC20Metadata(address(sale.token)).decimals())
+            .div(sale.tokenPriceInETH);
 
         // Must buy more than 0 tokens
         require(amountOfTokensBuying > 0, "Can't buy 0 tokens");
@@ -434,6 +434,10 @@ contract BrewerySale is ReentrancyGuard {
 
         // Increase amount of sold tokens
         sale.totalTokensSold = sale.totalTokensSold.add(amountOfTokensBuying);
+        require(
+            sale.totalTokensSold <= sale.amountOfTokensToSell,
+            "token is sold out!"
+        );
 
         // Increase amount of ETH raised
         sale.totalETHRaised = sale.totalETHRaised.add(msg.value);
@@ -444,10 +448,10 @@ contract BrewerySale is ReentrancyGuard {
 
         // Create participation object
         Participation memory p = Participation({
-        amountBought : amountOfTokensBuying,
-        amountETHPaid : msg.value,
-        timeParticipated : block.timestamp,
-        isPortionWithdrawn : _isPortionWithdrawn
+            amountBought: amountOfTokensBuying,
+            amountETHPaid: msg.value,
+            timeParticipated: block.timestamp,
+            isPortionWithdrawn: _isPortionWithdrawn
         });
 
         // Add participation for user.
@@ -475,13 +479,13 @@ contract BrewerySale is ReentrancyGuard {
 
         if (
             !p.isPortionWithdrawn[portionId] &&
-        vestingPortionsUnlockTime[portionId] <= block.timestamp
+            vestingPortionsUnlockTime[portionId] <= block.timestamp
         ) {
             p.isPortionWithdrawn[portionId] = true;
             uint256 amountWithdrawing = p
-            .amountBought
-            .mul(vestingPercentPerPortion[portionId])
-            .div(portionVestingPrecision);
+                .amountBought
+                .mul(vestingPercentPerPortion[portionId])
+                .div(portionVestingPrecision);
 
             // Withdraw percent which is unlocked at that portion
             if (amountWithdrawing > 0) {
@@ -494,24 +498,34 @@ contract BrewerySale is ReentrancyGuard {
     }
 
     // Expose function where user can withdraw multiple unlocked portions at once.
-    function withdrawMultiplePortions(uint256 [] calldata portionIds) external nonReentrant{
+    function withdrawMultiplePortions(uint256[] calldata portionIds)
+        external
+        nonReentrant
+    {
         uint256 totalToWithdraw = 0;
+        require(
+            block.timestamp >= sale.tokensUnlockTime,
+            "Tokens can not be withdrawn yet."
+        );
 
         Participation storage p = userToParticipation[msg.sender];
 
-        for (uint i = 0; i < portionIds.length; i++) {
+        for (uint256 i = 0; i < portionIds.length; i++) {
             uint256 portionId = portionIds[i];
-            require(portionId < vestingPercentPerPortion.length, "portion id is larger than portion length");
+            require(
+                portionId < vestingPercentPerPortion.length,
+                "portion id is larger than portion length"
+            );
 
             if (
                 !p.isPortionWithdrawn[portionId] &&
-            vestingPortionsUnlockTime[portionId] <= block.timestamp
+                vestingPortionsUnlockTime[portionId] <= block.timestamp
             ) {
                 p.isPortionWithdrawn[portionId] = true;
                 uint256 amountWithdrawing = p
-                .amountBought
-                .mul(vestingPercentPerPortion[portionId])
-                .div(portionVestingPrecision);
+                    .amountBought
+                    .mul(vestingPercentPerPortion[portionId])
+                    .div(portionVestingPrecision);
                 // Withdraw percent which is unlocked at that portion
                 totalToWithdraw = totalToWithdraw.add(amountWithdrawing);
             }
@@ -525,7 +539,7 @@ contract BrewerySale is ReentrancyGuard {
 
     // Internal function to handle safe transfer
     function safeTransferETH(address to, uint256 value) internal {
-        (bool success,) = to.call{value : value}(new bytes(0));
+        (bool success, ) = to.call{value: value}(new bytes(0));
         require(success);
     }
 
@@ -536,12 +550,12 @@ contract BrewerySale is ReentrancyGuard {
     }
 
     // Function to withdraw only earnings
-    function withdrawEarnings() external onlySaleOwner nonReentrant{
+    function withdrawEarnings() external onlySaleOwner nonReentrant {
         withdrawEarningsInternal();
     }
 
     // Function to withdraw only leftover
-    function withdrawLeftover() external onlySaleOwner nonReentrant{
+    function withdrawLeftover() external onlySaleOwner nonReentrant {
         withdrawLeftoverInternal();
     }
 
@@ -579,13 +593,12 @@ contract BrewerySale is ReentrancyGuard {
     /// @notice     Check signature user submits for registration.
     /// @param      signature is the message signed by the trusted entity (backend)
     /// @param      user is the address of user which is registering for sale
-    function checkRegistrationSignature(
-        bytes memory signature,
-        address user
-    ) public view returns (bool) {
-        bytes32 hash = keccak256(
-            abi.encodePacked(user, address(this))
-        );
+    function checkRegistrationSignature(bytes memory signature, address user)
+        public
+        view
+        returns (bool)
+    {
+        bytes32 hash = keccak256(abi.encodePacked(user, address(this)));
         bytes32 messageHash = hash.toEthSignedMessageHash();
         return admin.isAdmin(messageHash.recover(signature));
     }
@@ -596,14 +609,7 @@ contract BrewerySale is ReentrancyGuard {
         address user,
         uint256 amount
     ) public view returns (bool) {
-        return
-        admin.isAdmin(
-            getParticipationSigner(
-                signature,
-                user,
-                amount
-            )
-        );
+        return admin.isAdmin(getParticipationSigner(signature, user, amount));
     }
 
     /// @notice     Check who signed the message
@@ -615,34 +621,28 @@ contract BrewerySale is ReentrancyGuard {
         address user,
         uint256 amount
     ) public view returns (address) {
-        bytes32 hash = keccak256(
-            abi.encodePacked(
-                user,
-                amount,
-                address(this)
-            )
-        );
+        bytes32 hash = keccak256(abi.encodePacked(user, amount, address(this)));
         bytes32 messageHash = hash.toEthSignedMessageHash();
         return messageHash.recover(signature);
     }
 
     /// @notice     Function to get participation for passed user address
     function getParticipation(address _user)
-    external
-    view
-    returns (
-        uint256,
-        uint256,
-        uint256,
-        bool[] memory
-    )
+        external
+        view
+        returns (
+            uint256,
+            uint256,
+            uint256,
+            bool[] memory
+        )
     {
         Participation memory p = userToParticipation[_user];
         return (
-        p.amountBought,
-        p.amountETHPaid,
-        p.timeParticipated,
-        p.isPortionWithdrawn
+            p.amountBought,
+            p.amountETHPaid,
+            p.timeParticipated,
+            p.isPortionWithdrawn
         );
     }
 
@@ -653,15 +653,13 @@ contract BrewerySale is ReentrancyGuard {
 
     /// @notice     Function to get all info about vesting.
     function getVestingInfo()
-    external
-    view
-    returns (uint256[] memory, uint256[] memory)
+        external
+        view
+        returns (uint256[] memory, uint256[] memory)
     {
         return (vestingPortionsUnlockTime, vestingPercentPerPortion);
     }
 
     // Function to act as a fallback and handle receiving ETH.
-    receive() external payable {
-
-    }
+    receive() external payable {}
 }
