c4e30d18f8ac1c454f322e627a589cc562f62a23
J-Phx <zhiyu.wang@ecreditpal.com>
2022-03-20 14:27:38 +0800

    Add configuration information about the BSC network.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
-require('@nomiclabs/hardhat-waffle')
-require('@nomiclabs/hardhat-ethers')
-require("@nomiclabs/hardhat-web3")
-require('@openzeppelin/hardhat-upgrades')
+require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-ethers');
+require("@nomiclabs/hardhat-web3");
+require('@openzeppelin/hardhat-upgrades');
+require("@nomiclabs/hardhat-etherscan");
 require('solidity-coverage');
-require("dotenv").config()
 
 
 /**
@@ -27,6 +27,16 @@ module.exports = {
             url: 'https://eth-rinkeby.alchemyapi.io/v2/e3DsrbyScPv5XXuEeuvti7Li2vd6o6bm',
             accounts: [process.env.PK],
         },
+        bsc_testnet: {
+            url: 'https://data-seed-prebsc-1-s1.binance.org:8545',
+            chainId: 97,
+            accounts: [process.env.PK],
+        },
+        bsc_mainnet: {
+            url: 'https://bsc-dataseed1.ninicoin.io',
+            chainId: 56,
+            accounts: [process.env.PK],
+        },
         local: {
             url: 'http://localhost:8545',
         },
@@ -39,6 +49,16 @@ module.exports = {
                 runs: 200,
             },
         },
+    },
+    contractSizer: {
+        alphaSort: true,
+        runOnCompile: true,
+        disambiguatePaths: false,
+    },
+    etherscan: {
+        apiKey: {
+            bsc: 'P9KWHHWMIMS8WYYM9H8WNU648DVUGKPT8N'
+        }
     },
 };
 
