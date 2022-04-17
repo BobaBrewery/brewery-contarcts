84d9affd939076e7220c3af9c8d1138870703cf9
ouzm <wjj315315@gmail.com>
2022-04-17 16:35:54 +0800

    add sig in mint

--- contracts/sales/NFTMinter.sol
+++ contracts/sales/NFTMinter.sol
@@ -6,9 +6,11 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./interface/IMedievalNFT.sol";
+import "../interfaces/IAdmin.sol";
 
-contract NFTMinter is ReentrancyGuard, Ownable {
+contract NFTMinter is ReentrancyGuard {
     IMedievalNFT immutable public nft;
+    IAdmin public admin;
 
     //price
     uint256 public ethAmount;
@@ -17,20 +19,36 @@ contract NFTMinter is ReentrancyGuard, Ownable {
 
     event UseReferalCode(uint256 quantity, bytes32 code);
 
+    modifier onlyAdmin() {
+        require(
+            admin.isAdmin(msg.sender),
+            "Only admin can call this function."
+        );
+        _;
+    }
+
     constructor (
+        address _admin,
         address _nft
     ) public {
+        require(_admin != address(0), "_admin != address(0)");
+        admin = IAdmin(_admin);
         nft = IMedievalNFT(_nft);
         maxQuantity = 5;
     }
 
-    function mint(uint256 quantity, bytes32 code) external nonReentrant payable {
+    function mint(uint256 quantity, bytes32 code, bytes memory signature) external nonReentrant payable {
         require(quantity <= maxQuantity, "Exceed Max Quantity");
         require(counter >= quantity, "The current batch has been sold out!");
 
         uint256 value = msg.value;
-
         require(value >= ethAmount * quantity, "Incorrect ETH Amount.");
+
+        require(
+            checkParticipationSignature(signature, msg.sender, amount),
+            "Invalid signature. Verification failed"
+        );
+
         payable(address(this)).transfer(value);
 
         nft.mint(msg.sender, quantity);
@@ -44,24 +62,46 @@ contract NFTMinter is ReentrancyGuard, Ownable {
         require(success);
     }
 
+    // Function to check if admin was the message signer
+    function checkParticipationSignature(
+        bytes memory signature,
+        address user,
+        uint256 amount
+    ) public view returns (bool) {
+        return admin.isAdmin(getParticipationSigner(signature, user, amount));
+    }
+
+    /// @notice     Check who signed the message
+    /// @param      signature is the message allowing user to participate in sale
+    /// @param      user is the address of user for which we're signing the message
+    /// @param      amount is the maximal amount of tokens user can buy
+    function getParticipationSigner(
+        bytes memory signature,
+        address user,
+        uint256 amount
+    ) public view returns (address) {
+        bytes32 hash = keccak256(abi.encodePacked(user, amount, address(this)));
+        bytes32 messageHash = hash.toEthSignedMessageHash();
+        return messageHash.recover(signature);
+    }
+
     // function to withdraw earnings
-    function withdrawEarnings() public onlyOwner {
+    function withdrawEarnings() public onlyAdmin {
         // transfer
         safeTransferETH(msg.sender, address(this).balance);
     }
 
-
     function setPrice(
-        uint256 _ethAmount, uint256 _counter) external onlyOwner {
+        uint256 _ethAmount, uint256 _counter) external onlyAdmin {
         ethAmount = _ethAmount;
         counter = _counter;
     }
 
-    function setMaxQuantity(uint256 _maxQuantity) external onlyOwner {
+    function setMaxQuantity(uint256 _maxQuantity) external onlyAdmin {
         maxQuantity = _maxQuantity;
     }
 
-    function getBalance() public view returns(uint){
+    function getBalance() public view returns (uint){
         return address(this).balance;
     }
 
