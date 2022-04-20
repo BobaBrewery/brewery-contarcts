81b1dc33437e05831a693415da55171b971d9fe7
ouzm <wjj315315@gmail.com>
2022-04-20 14:50:04 +0800

    Fix batch mint

--- contracts/mock/MedievalNFT.sol
+++ contracts/mock/MedievalNFT.sol
@@ -5,15 +5,23 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract MedievalNFT is ERC721, AccessControl {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
     bytes32 private constant NFT_MINTER_ROLE = keccak256("NFT_MINTER_ROLE");
 
     constructor() ERC721("MedievalNFT", "NFT") public {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
-
-    function mint(address to, uint256 tokenId) public {
+    function mint(address to, uint256 quantity) public {
         require(hasRole(NFT_MINTER_ROLE, msg.sender), "Caller is not a minter");
-        _safeMint(to, tokenId, "");
+        require(quantity >= 1, "quantity should over 1");
+
+        while (quantity != 0) {
+            quantity -= 1;
+            _tokenIds.increment();
+            uint256 newItemId = _tokenIds.current();
+            _mint(to, newItemId);
+        }
     }
 }
