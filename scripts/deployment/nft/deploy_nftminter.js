ce0bcdc864f7ad56fb1ecbb31734a2e4917e74f2
ouzm <wjj315315@gmail.com>
2022-04-21 15:50:42 +0800

    abi update

--- scripts/deployment/nft/deploy_nftminter.js
+++ scripts/deployment/nft/deploy_nftminter.js
@@ -4,7 +4,6 @@ const {saveContractAddress, getSavedContractAddresses} = require('../../utils')
 
 const MINT_ROLE_BYTES32 = hre.web3.utils.keccak256(hre.web3.utils.asciiToHex("NFT_MINTER_ROLE"))
 const COUNTER = 2000
-const PRICE = hre.ethers.utils.parseEther("0.1")
 const ADMINS = [
     "0xe6CEf4eE7c2757dF130a63e85a3849Ec56E812ef",
     "0x0f590970a45d0b4c2dcfcaFF453400eE9B91B317"
@@ -14,6 +13,7 @@ async function main() {
     const contracts = getSavedContractAddresses()[hre.network.name];
     const AdminFactory = await ethers.getContractFactory("Admin");
     let admin_contract = await AdminFactory.deploy(ADMINS);
+    console.log('admin deployed to :', admin_contract.address)
     saveContractAddress(hre.network.name, "Admin", admin_contract.address);
     const Minter = await hre.ethers.getContractFactory("NFTMinter");
     const nft = await hre.ethers.getContractAt("MedievalNFT", contracts["MedievalNFT"])
@@ -22,10 +22,12 @@ async function main() {
     console.log("Minter deployed to: ", minter.address);
     saveContractAddress(hre.network.name, "NFTMinter", minter.address);
 
-    await minter.setPrice(PRICE, COUNTER)
+    await minter.setBatchCounter(COUNTER)
+    console.log('Batch count set')
 
     // grant mint role
     await nft.grantRole(MINT_ROLE_BYTES32, minter.address);
+    console.log('mint role granted')
 }
 
 
