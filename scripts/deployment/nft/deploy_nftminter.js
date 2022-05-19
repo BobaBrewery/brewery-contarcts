aebdbb411a296295b5aceb316c8c26507311fc33
ouzm <wjj315315@gmail.com>
2022-05-19 16:28:23 +0800

    update deploy nft minter

--- scripts/deployment/nft/deploy_nftminter.js
+++ scripts/deployment/nft/deploy_nftminter.js
@@ -5,20 +5,25 @@ const {saveContractAddress, getSavedContractAddresses} = require('../../utils')
 const MINT_ROLE_BYTES32 = hre.web3.utils.keccak256(hre.web3.utils.asciiToHex("NFT_MINTER_ROLE"))
 const COUNTER = 300
 const ADMINS = [
-    "0xe6CEf4eE7c2757dF130a63e85a3849Ec56E812ef",
-    "0x0f590970a45d0b4c2dcfcaFF453400eE9B91B317"
+    //eth mainnet
+    "0x7210D475C0F2e622de6CFbD1AFF94A0C2fA12fDE",
+    "0x71C4a2860278AF34752A235581cA766df4207318"
+    // test
+    // "0xe6CEf4eE7c2757dF130a63e85a3849Ec56E812ef",
+    // "0x0f590970a45d0b4c2dcfcaFF453400eE9B91B317"
 ]
 
+const NFT_ADDR = "0xd0665D59Eb133b7BAec5c41EB48C773F0Fc1dF46"
+
 async function main() {
-    const contracts = getSavedContractAddresses()[hre.network.name];
     const AdminFactory = await ethers.getContractFactory("Admin");
     let admin_contract = await AdminFactory.deploy(ADMINS);
     console.log('admin deployed to :', admin_contract.address)
     saveContractAddress(hre.network.name, "Admin", admin_contract.address);
     const Minter = await hre.ethers.getContractFactory("NFTMinter");
-    const nft = await hre.ethers.getContractAt("MedievalNFT", contracts["MedievalNFT"])
+    // const nft = await hre.ethers.getContractAt("MedievalNFT", contracts["MedievalNFT"])
     console.log('start deploy minter...')
-    const minter = await Minter.deploy(admin_contract.address, nft.address);
+    const minter = await Minter.deploy(admin_contract.address, NFT_ADDR);
     await minter.deployed();
     console.log("Minter deployed to: ", minter.address);
     saveContractAddress(hre.network.name, "NFTMinter", minter.address);
@@ -27,8 +32,8 @@ async function main() {
     console.log('Batch count set')
 
     // grant mint role
-    await nft.grantRole(MINT_ROLE_BYTES32, minter.address);
-    console.log('mint role granted')
+    // await nft.grantRole(MINT_ROLE_BYTES32, minter.address);
+    // console.log('mint role granted')
 }
 
 
