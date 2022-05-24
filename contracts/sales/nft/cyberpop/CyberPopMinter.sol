9a3c366442463bca30a78e36ada398d4ff862f5a
ouzm <wjj315315@gmail.com>
2022-05-24 14:46:54 +0800

    update cyberpop minter

--- contracts/sales/nft/cyberpop/CyberPopMinter.sol
+++ contracts/sales/nft/cyberpop/CyberPopMinter.sol
@@ -12,15 +12,19 @@ contract CyberPopMinter is ReentrancyGuard {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
-    IERC1155NFTPublic public immutable nft;
+    IERC1155NFTPublic public immutable roleNft;
+    IERC1155NFTPublic public immutable propNft;
     IAdmin public admin;
-    IERC20 public immutable USDT;
+    IERC20 public USDT;
     uint256 public immutable rolePrice = 49000000000000000000;
     uint256 public totalTokensDeposited;
 
+    event RoleMinted(address buyer, uint256 amount);
+    event propMinted(address buyer, uint256 amount);
 
     // left nft amount
-    mapping(uint256 => uint256) public counters;
+    mapping(uint256 => uint256) public roleCounters;
+    mapping(uint256 => uint256) public propCounters;
 
     modifier onlyAdmin() {
         require(
@@ -30,12 +34,29 @@ contract CyberPopMinter is ReentrancyGuard {
         _;
     }
 
-    constructor(address _admin, address _nft, address _usdt) public {
+    constructor(address _admin, address _propNft, address _roleNft, address _usdt) public {
         require(_admin != address(0), "_admin != address(0)");
         admin = IAdmin(_admin);
-        nft = IERC1155NFTPublic(_nft);
-        counters[2] = 50;
+        roleNft = IERC1155NFTPublic(_roleNft);
+        propNft = IERC1155NFTPublic(_propNft);
+        roleCounters[2] = 2000;
+        propCounters[2] = 2000;
         USDT = IERC20(_usdt);
+    }
+
+    function freeMint(
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    ) external nonReentrant {
+        require(propCounters[id] >= amount, "The current batch has been sold out!");
+        require(
+            checkMintSignature(signature, msg.sender, 0, amount),
+            "Invalid mint signature. Verification failed"
+        );
+        propCounters[id] = propCounters[id].sub(amount);
+        propNft.mint(msg.sender, id, amount, data);
+        emit propMinted(msg.sender, amount);
     }
 
     function mint(
@@ -43,68 +64,54 @@ contract CyberPopMinter is ReentrancyGuard {
         uint256 amount,
         bytes memory data
     ) external nonReentrant {
-        require(counters[id] >= amount, "The current batch has been sold out!");
+        require(roleCounters[id] >= amount, "The current batch has been sold out!");
         USDT.safeTransferFrom(msg.sender, address(this), rolePrice);
         totalTokensDeposited += rolePrice;
 
-        // require(
-        //     checkMintSignature(signature, msg.sender, 0, 1),
-        //     "Invalid mint signature. Verification failed"
-        // );
-        counters[id] = counters[id].sub(amount);
-        nft.mint(msg.sender, id, amount, data);
+        require(
+            checkMintSignature(signature, msg.sender, 0, amount),
+            "Invalid mint signature. Verification failed"
+        );
+        roleCounters[id] = roleCounters[id].sub(amount);
+        roleNft.mint(msg.sender, id, amount, data);
+        emit RoleMinted(msg.sender, amount);
     }
 
-    function mintBatch(
-        uint256[] memory ids,
-        uint256[] memory amounts,
-        bytes memory data
-    ) external nonReentrant {
-        require(ids.length == amounts.length, "Incorrect entry format.");
-        for (uint256 i = 0; i < ids.length; i++) {
-            uint256 id = ids[i];
-            uint256 amount = amounts[i];
-            require(
-                counters[id] >= amount,
-                "The current batch has been sold out!"
-            );
-            counters[id] = counters[id].sub(amount);
-        }
-
-        nft.mintBatch(msg.sender, ids, amounts, data);
-    }
     // function to withdraw earnings
-    function withdrawEarnings() public onlyAdmin {
+    function withdrawEarnings(uint _amount) public onlyAdmin {
         uint256 balance = USDT.balanceOf(address(this));
-        USDT.safeTransfer(msg.sender, balance);
+        if (_amount > balance) {
+            USDT.safeTransfer(address(msg.sender), balance);
+        } else {
+            USDT.safeTransfer(address(msg.sender), _amount);
+        }
     }
 
+    // Function to check if admin was the message signer
+    function checkMintSignature(
+        bytes memory signature,
+        address user,
+        uint256 price,
+        uint256 amount
+    ) public view returns (bool) {
+        return admin.isAdmin(getMintSigner(signature, user, price, amount));
+    }
 
-    // // Function to check if admin was the message signer
-    // function checkMintSignature(
-    //     bytes memory signature,
-    //     address user,
-    //     uint256 price,
-    //     uint256 amount
-    // ) public view returns (bool) {
-    //     return admin.isAdmin(getMintSigner(signature, user, price, amount));
-    // }
-
-    // /// @notice     Check who signed the message
-    // /// @param      signature is the message allowing user to participate in sale
-    // /// @param      user is the address of user for which we're signing the message
-    // function getMintSigner(
-    //     bytes memory signature,
-    //     address user,
-    //     uint256 price,
-    //     uint256 amount
-    // ) public view returns (address) {
-    //     bytes32 hash = keccak256(
-    //         abi.encodePacked(user, price, amount, address(this))
-    //     );
-    //     bytes32 messageHash = hash.toEthSignedMessageHash();
-    //     return messageHash.recover(signature);
-    // }
+    /// @notice     Check who signed the message
+    /// @param      signature is the message allowing user to participate in sale
+    /// @param      user is the address of user for which we're signing the message
+    function getMintSigner(
+        bytes memory signature,
+        address user,
+        uint256 price,
+        uint256 amount
+    ) public view returns (address) {
+        bytes32 hash = keccak256(
+            abi.encodePacked(user, price, amount, address(this))
+        );
+        bytes32 messageHash = hash.toEthSignedMessageHash();
+        return messageHash.recover(signature);
+    }
 
     receive() external payable {}
 }
