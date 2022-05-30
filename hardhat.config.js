4e12fd5975fb17fdd804d9f20e0d5452b4e1cc45
J-Phx <zhiyu.wang@ecreditpal.com>
2022-05-30 17:24:06 +0800

    Modify rewardFund script

--- hardhat.config.js
+++ hardhat.config.js
@@ -12,21 +12,25 @@ require("dotenv").config()
  */
 module.exports = {
     networks: {
+        ethereum: {
+            url: 'https://mainnet.infura.io/v3/eaa5fb64cc5d4f43aa01d12ead1602f3',
+            accounts: [process.env.PK],
+        },
+        rinkeby: {
+            url: 'https://rinkeby.infura.io/v3/3d0da1b673d249078956653f2d65bd75',
+            gas: 2100000,
+            gasPrice: 100000000000,
+            accounts: [process.env.PK],
+        },
         boba_mainnet: {
             url: 'https://mainnet.boba.network/',
             chainId: 288,
-            gas: 5000000,
             accounts: [process.env.PK],
         },
         boba_rinkeby: {
             url: 'https://rinkeby.boba.network',
-            gas: 5000000,
-            gasPrice: 1000000000,
-            accounts: [process.env.PK],
-        },
-        rinkeby: {
-            url: 'https://eth-rinkeby.alchemyapi.io/v2/e3DsrbyScPv5XXuEeuvti7Li2vd6o6bm',
             gas: 21000000,
+            gasPrice: 1000000000,
             accounts: [process.env.PK],
         },
         bsc_mainnet: {
@@ -35,10 +39,23 @@ module.exports = {
             accounts: [process.env.PK],
         },
         bsc_testnet: {
-            url: 'https://data-seed-prebsc-1-s1.binance.org:8545',
+            url: 'https://data-seed-prebsc-2-s3.binance.org:8545/',
             gas: 21000000,
             chainId: 97,
             accounts: [process.env.PK],
+        },
+        fuji: {
+            url: 'https://api.avax-test.network/ext/bc/C/rpc',
+            gasPrice: 225000000000,
+            chainId: 43113,
+            accounts: [process.env.PK]
+        },
+        avax_mainnet: {
+            url: 'https://api.avax.network/ext/bc/C/rpc',
+            gasPrice: 50000000000,
+            chainId: 43114,
+            timeout: 900000000,
+            accounts: [process.env.PK]
         },
         local: {
             url: 'http://localhost:8545',
--- scripts/deployment/deploy_init_4_rewardFund.js
+++ scripts/deployment/deploy_init_4_rewardFund.js
@@ -1,15 +1,17 @@
 const hre = require("hardhat");
-const {getSavedContractAddresses } = require('../utils')
+const { getSavedContractAddresses } = require('../utils');
+const config = require('../configs/config.json');
 
 // Call using the private key of the owner's address of the BRE token
 async function main() {
+    const c = config[hre.network.name];
     const contracts = getSavedContractAddresses()[hre.network.name];
 
     const allocationStaking = await hre.ethers.getContractAt('AllocationStaking', contracts['AllocationStakingProxy']);
-    
+
     // Fund
-    await allocationStaking.fund(ethers.utils.parseEther('20000000'));
-    console.log('Funded 20000000 tokens')
+    await allocationStaking.fund(ethers.utils.parseEther(c.initialRewardsAllocationStaking));
+    console.log(`Funded ${c.initialRewardsAllocationStaking} tokens`);
 }
 
 
