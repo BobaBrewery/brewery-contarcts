069ace38b6333ef06db38f21ef244adf43675ca9
J-Phx <zhiyu.wang@ecreditpal.com>
2022-06-29 13:18:28 +0800

    Add Cyberpop refund ABI

--- scripts/deployment/nft/cyberpop/deploy_refund.js
+++ scripts/deployment/nft/cyberpop/deploy_refund.js
@@ -3,7 +3,7 @@ const { ethers } = require("hardhat");
 const { saveContractAddress, getSavedContractAddresses } = require('../../../utils')
 
 
-const USDT_HOLDER_ADDR = ""
+const USDT_HOLDER_ADDR = "0x24e1Ae757ca647101B0e7614217b7AE26338F578"
 const NFT_RECIPIENT_ADDR = "0x7291030263771b40731D6Bc6b352358D23F5737F"
 const ROLE_NFT = "0x10fdE59432D1d6eE7aD25448e3D8B9b3D2c08b89"
 const BSC_USDT = "0x55d398326f99059fF775485246999027B3197955"
