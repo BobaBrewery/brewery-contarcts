570d2d3b02e7712409831557c1992a84741b0f2d
J-Phx <zhiyu.wang@ecreditpal.com>
2022-07-27 10:35:03 +0800

    Fix-Metaderby: compile error And deploy to rinkeby

--- contracts/sales/nft/metaderby/HorseRace.sol
+++ contracts/sales/nft/metaderby/HorseRace.sol
@@ -2,11 +2,14 @@
 pragma solidity 0.6.12;
 
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
+import "@openzeppelin/contracts/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/Pausable.sol";
 import "../../../interfaces/IAdmin.sol";
 
 contract HorseRace is ReentrancyGuard, Pausable {
+    using ECDSA for bytes32;
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
