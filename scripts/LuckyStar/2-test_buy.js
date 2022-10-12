a38d643306531ce8a3186082a7224bf69a9fc469
J-Phx <zhiyu.wang@ecreditpal.com>
2022-10-12 12:27:28 +0800

    Update

--- scripts/LuckyStar/2-test_buy.js
+++ scripts/LuckyStar/2-test_buy.js
@@ -1,4 +1,5 @@
 const { ethers } = require("hardhat");
+const { BigNumber } = require("ethers");
 const { saveContractAddress, getSavedContractAddresses } = require('../utils');
 
 
@@ -6,12 +7,14 @@ async function main() {
 
     const contracts = getSavedContractAddresses()[hre.network.name];
 
-    const token = await ethers.getContractAt('BreToken', contracts['MOCK-TOKEN']);
+    const token = await ethers.getContractAt('BreToken', contracts['BOBA-MOCK-TOKEN']);
     console.log("MOCK-TOKEN contract address: ", token.address);
     const LuckyStar = await ethers.getContractAt('LuckyStar', contracts['LuckyStar']);
     console.log("LuckyStar contract address: ", LuckyStar.address);
 
-    await token.approve(LuckyStar.address, 10000000000)
+    const NUMBER_1E18 = "1000000000000000000";
+    const approveAmount = BigNumber.from(10000).mul(NUMBER_1E18);
+    await token.approve(LuckyStar.address, approveAmount)
 
     await LuckyStar.buy(5, 10, 5);
 
--- scripts/deployment/deploy_luckyStar.js
+++ scripts/deployment/deploy_luckyStar.js
@@ -8,7 +8,7 @@ async function main() {
     const contracts = getSavedContractAddresses()[hre.network.name];
 
     const FUNDER_ADDRESS = "0x0f590970a45d0b4c2dcfcaFF453400eE9B91B317";
-    const TOKEN_ADDRESS = "0xa6518818DE27c355153976A1e631Da77F11b3E56";
+    const TOKEN_ADDRESS = "0xf8BB8E60A211ebaF5876D03767F2E378d49D289d";
 
     const Admin = await hre.ethers.getContractAt('Admin', contracts['Admin']);
     console.log("Admin contract deployed to: ", Admin.address);
